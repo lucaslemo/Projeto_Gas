441139086b3b2333a9ce23a6b5f3e10bba34da29
lucaslemo <lucaslemo852@gmail.com>
2022-08-16 14:36:47 -0300

    Conexao ao banco pelo pdo_mysql

--- src/Infra/Persistence/ConnectionCreator.php
+++ src/Infra/Persistence/ConnectionCreator.php
@@ -2,14 +2,27 @@
 
 namespace Poligas\Aplicacao\Infra\Persistence;
 
-Class ConnectionCreator{
-
+Class ConnectionCreator
+{
     public static function createConnection(): \PDO
     {
-        $path = __DIR__ . '/db.sqlite';
-        $connection =  new \PDO('sqlite:' . $path);
-        $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        try {
+            // Conexao com o mysql
+            $host = 'localhost';
+            $dbName = 'poligasdb';
+            $username = 'root';
+            $password = '';
+            $connection =  new \PDO(
+                'mysql:host=' . $host . ';dbname=' . $dbName, 
+                $username, 
+                $password
+            );
+            $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
-        return $connection;
+            return $connection;
+        } catch (\PDOException $e) {
+            echo $e->getMessage() . '<br>';
+            echo $e;
+        }
     }
 }
